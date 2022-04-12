a16af1ecb5ff4f3ef6eb9d0b16ee5b1a5420944c
vader jeon <mazingazz@gmail.com>
2022-04-13 08:18:40 +0900

    2022.04.13 01

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import type { NextPage } from "next";
-import { useQuery } from "@apollo/react-hooks"
-import gql from "graphql-tag"
-import { FormattedMessage } from "react-intl";
+import type { NextPage } from 'next'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import { FormattedMessage } from 'react-intl'
+import utilStyles from '../styles/utils.module.css'
+
+import type { ReactElement, ReactNode } from 'react'
 
 type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
+  getLayout?: (page: ReactElement) => ReactNode
+}
 
 const GET_BBS = gql`
   {
@@ -25,11 +28,9 @@ const Home: NextPageWithLayout = () => {
     <>
       <FormattedMessage id="app.content" defaultMessage="Learn React" />
       <section className={utilStyles.headingMd}></section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        INDEX PAGE
-      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>INDEX PAGE</section>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
