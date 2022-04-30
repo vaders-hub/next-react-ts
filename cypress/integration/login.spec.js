19a1f041e837c00b1e50ad106496b8afb7846f20
hyunbae.jeon <mazingazz@gmail.com>
2022-04-30 16:05:16 +0900

    2022.04.30 01

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -15,23 +15,24 @@ describe('visit login page', () => {
   })
 
   it('enter id and password', () => {
-    enterId('test')
-    enterPw('1234')
-  })
+    enterId(Cypress.env('userName'))
+    enterPw(Cypress.env('userPassword'))
+    
+    cy
+      .get('main button')
+      .click() 
+      .request('POST', '/api/members/signin', {
+        memid: Cypress.env('userName'),
+        mempw: Cypress.env('userPassword'),
+      }).then((response) => {
+        expect(response.status).to.eq(200)
 
-  it('cy.request() - save response in the shared test context', () => {
-    cy.request('POST', '/api/members/signin', {
-      memid: 'test',
-      mempw: '1234',
-    }).then((response) => {
-      expect(response.status).to.eq(200)
-
-      cy.window()
-        .its('store')
-        .invoke('getState')
-        .then((state) => {
-          expect(state.member.signedIn).equal(false, true)
-        })
-    })
+        cy.window()
+          .its('store')
+          .invoke('getState')
+          .then((state) => {
+            expect(state.member.signedIn).equal(true)
+          })
+      })
   })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,13 +1,9 @@
 module.exports = (on, config) => {
-  // modify the config values
+
   config.defaultCommandTimeout = 10000
 
-  // read an environment variable and
-  // pass its value to the specs
-  config.env.userName = process.env.TEST_USER || 'Joe'
-  // the specs will be able to access the above value
-  // by using Cypress.env('userName')
+  config.env.userName = process.env.TEST_USER || 'test'
+  config.env.userPassword = process.env.TEST_PASSWORD || '1234'
 
-  // IMPORTANT return the updated config object
   return config
 }
